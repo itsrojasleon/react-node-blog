6a94a1819c9e489e07baf0b42c021b37e2c9cd06
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-10 18:02:40 -0500

    Add singin and signout routes

---
+++ blog-server/routes/authRoutes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = mongoose.model('User');
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
+});
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (!user) {
+      return res.status(422).send({ error: 'Invalid password or email' });
+    }
+
+    try {
+      user.comparePassword(password);
+      const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+      res.send({ token });
+    } catch (err) {
+      return res.status(422).send({ error: 'Invalid password or email' });
+    }
+  });
+});
+
+module.exports = router;
